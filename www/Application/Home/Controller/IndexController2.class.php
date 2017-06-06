29d426883333de59d24279706501ac9ff87df21e
thundersword <41542066@qq.com>
2017-06-06 14:30:24 +0800

    debug session 5

--- www/Application/Home/Controller/IndexController2.class.php
+++ www/Application/Home/Controller/IndexController2.class.php
@@ -2,32 +2,11 @@
 
 namespace Home\Controller;
 
-use Think\Controller;
-
-class IndexController extends Controller {
+class IndexController extends CommonController  {
 
     public function _initialize()
     {
-        $cateModel = M('softs_cate');
-        $cate_c = $cateModel->where(array('id' => $cate_id))->find();
-        if($cate_c['pid']){
-            $pid = $cateModel->where(array('id' => $cate_c['pid']))->find();
-            $this->assign('pid',$pid);
-        }
-        $map_f['pid'] = 0;
-        $map_s['pid'] = array('neq', 0);
-        $cate = $cateModel->where($map_f)->select();
-        $son = $cateModel->where($map_s)->select();
-        foreach ($son as $k => $v) {
-            foreach ($cate as $m => $n) {
-                if ($v['pid'] == $n['id']) {
-                    $cate[$m]['child'][] = $v;
-                }
-            }
-        }
-        $site_config = M('setting')->select();
-        $this->assign('cate',$cate);
-        $this->assign('site_config',$site_config);
+        parent::initialize();
     }
 
 
